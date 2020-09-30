4a8e340e3c093a3cad6c8ccd04a6939896b5d1ef
Jan Rummens <Jan.Rummens@digipolis.be>
2020-10-01 01:38:27 +0200

    changed to localhost

--- selenium_search.py
+++ selenium_search.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 capabilities = options.to_capabilities()
 driver = webdriver.Remote(
-    command_executor='http://127.0.0.1:4444/wd/hub',
+    command_executor='http://localhost:4444/wd/hub',
     desired_capabilities=capabilities)
 driver.maximize_window()
 
